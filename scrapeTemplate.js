5dc9fd1959f4927e4c09619bfa4b3290ac7af1c4
Nguyễn Chánh Đạt <154238663+NguyenChanhDat@users.noreply.github.com>
2024-03-11 09:36:02 +0700

    Update and rename scrapePlayerData.js to scrapeTemplate.js
    Add some note, rename to template

--- scrapeTemplate.js
+++ scrapeTemplate.js
@@ -1,15 +1,15 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 axios
-  .get("https://footystats.org/clubs/dpm-nam-dinh-fc-4019#")
+  .get("***")//the address of the website u wanna scrape data
   .then(({ data }) => {
     const $ = cheerio.load(data);
 
-    const players = $("p.col-lg-6.ellipses")
+    const players = $("***")//the elements' parent's class
       .map((_, player) => {
         const $player = $(player);
-        const name = $player.find(".semi-bold").text();
-        const position = $player.find(".col-lg-2.ac.fs09e").text();
+        const name = $player.find("*****").text();//the players name element's class
+        const position = $player.find(".col-lg-2.ac.fs09e").text();//the players position element's class
         return { name: name, position: position };
       })
       .toArray();
