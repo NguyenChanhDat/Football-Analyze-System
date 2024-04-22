e47ca19a7e6595b53bd37a0b36d976c83f73d8ce
nguyenchanhdat <chanhdat2504@gmail.com>
2024-04-22 11:08:20 +0700

    successfully added teamsData into mysql

--- BE/Premier League/scrapeTeamData.js
+++ BE/Premier League/scrapeTeamData.js
@@ -6,7 +6,7 @@ const con = mysql.createPool({
   host: "localhost",
   user: "sqluser",
   password: "dat20112011",
-  database: "todolistdb",
+  database: "epl",
 });
 
 axios.get("https://footystats.org/england/premier-league#").then(({ data }) => {
@@ -46,22 +46,41 @@ axios.get("https://footystats.org/england/premier-league#").then(({ data }) => {
   let teamsGraph = new TeamGraph();
   for (let i = 0; i < teams.length; i++) {
     teamsGraph.addTeam(teams[i].name);
-    teamsGraph.teams[teams[i].name].addMetric(
-      "matchplayed",
-      teams[i].matchplayed
-    );
-    teamsGraph.teams[teams[i].name].addMetric("win", teams[i].win);
-    teamsGraph.teams[teams[i].name].addMetric("draw", teams[i].draw);
-    teamsGraph.teams[teams[i].name].addMetric("loss", teams[i].loss);
-    teamsGraph.teams[teams[i].name].addMetric("goal", teams[i].goal);
-    teamsGraph.teams[teams[i].name].addMetric(
-      "goalConceded",
-      teams[i].goalConceded
-    );
-    teamsGraph.teams[teams[i].name].addMetric(
+    teamsGraph.teams[i].addMetric("matchplayed", teams[i].matchplayed);
+    teamsGraph.teams[i].addMetric("win", teams[i].win);
+    teamsGraph.teams[i].addMetric("draw", teams[i].draw);
+    teamsGraph.teams[i].addMetric("loss", teams[i].loss);
+    teamsGraph.teams[i].addMetric("goal", teams[i].goal);
+    teamsGraph.teams[i].addMetric("goalConceded", teams[i].goalConceded);
+    teamsGraph.teams[i].addMetric(
       "goalDifference",
       teams[i].goal - teams[i].goalConceded
     );
   }
-  console.log(teamsGraph);
+
+  con.getConnection(function (err, connection) {
+    if (err) throw err;
+    console.log("Connected!");
+    for (let i = 0; i < teamsGraph.teams.length; i++) {
+      connection.query(
+        "INSERT INTO teamsmetrics (teamName, matchplayed, win, draw, loss, goal, goalConceded) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        [
+          teamsGraph.teams[i].name,
+          teamsGraph.teams[i].metrics.matchplayed,
+          teamsGraph.teams[i].metrics.win,
+          teamsGraph.teams[i].metrics.draw,
+          teamsGraph.teams[i].metrics.loss,
+          teamsGraph.teams[i].metrics.goal,
+          teamsGraph.teams[i].metrics.goalConceded,
+        ], // Pass both todo and date values as an array
+        function (err, result) {
+          if (err) {
+            connection.release(); // Release the connection if there's an error
+            throw err;
+          }
+          console.log("Data added to teamsmetrics in mySQL server!");
+        }
+      );
+    }
+  });
 });
