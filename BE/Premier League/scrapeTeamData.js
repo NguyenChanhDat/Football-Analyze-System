d9cb29bafb7e667c078442c6f959b41bae98aca4
nguyenchanhdat <chanhdat2504@gmail.com>
2024-04-22 10:14:56 +0700

    added teams data to mysql

--- BE/Premier League/scrapeTeamData.js
+++ BE/Premier League/scrapeTeamData.js
@@ -1,6 +1,14 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const TeamGraph = require("../TeamClass.js");
+const mysql = require("mysql");
+const con = mysql.createPool({
+  host: "localhost",
+  user: "sqluser",
+  password: "dat20112011",
+  database: "todolistdb",
+});
+
 axios.get("https://footystats.org/england/premier-league#").then(({ data }) => {
   const $ = cheerio.load(data);
 
@@ -8,9 +16,22 @@ axios.get("https://footystats.org/england/premier-league#").then(({ data }) => {
     .map((_, team) => {
       const $team = $(team);
       const name = $team.find("td.team.borderRightContent").text();
+      const matchplayed = $team.find("td.mp").text();
+      const win = $team.find("td.win").text();
+      const draw = $team.find("td.draw").text();
+      const loss = $team.find("td.loss").text();
       const goal = $team.find("td.gf").text();
       const goalConceded = $team.find("td.ga").text();
-      return { name: name, goal: goal, goalConceded: goalConceded };
+
+      return {
+        name: name,
+        matchplayed: matchplayed,
+        win: win,
+        draw: draw,
+        loss: loss,
+        goal: goal,
+        goalConceded: goalConceded,
+      };
     })
     .toArray();
 
@@ -21,6 +42,26 @@ axios.get("https://footystats.org/england/premier-league#").then(({ data }) => {
   //     teams.splice(i, 1);
   //   }
   // }
-  console.log(teams);
+  // console.log(teams);
   let teamsGraph = new TeamGraph();
+  for (let i = 0; i < teams.length; i++) {
+    teamsGraph.addTeam(teams[i].name);
+    teamsGraph.teams[teams[i].name].addMetric(
+      "matchplayed",
+      teams[i].matchplayed
+    );
+    teamsGraph.teams[teams[i].name].addMetric("win", teams[i].win);
+    teamsGraph.teams[teams[i].name].addMetric("draw", teams[i].draw);
+    teamsGraph.teams[teams[i].name].addMetric("loss", teams[i].loss);
+    teamsGraph.teams[teams[i].name].addMetric("goal", teams[i].goal);
+    teamsGraph.teams[teams[i].name].addMetric(
+      "goalConceded",
+      teams[i].goalConceded
+    );
+    teamsGraph.teams[teams[i].name].addMetric(
+      "goalDifference",
+      teams[i].goal - teams[i].goalConceded
+    );
+  }
+  console.log(teamsGraph);
 });
