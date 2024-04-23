0bd8b194eafd735a9f160d16f94facb2e3210cc5
nguyenchanhdat <chanhdat2504@gmail.com>
2024-04-23 22:09:38 +0700

    added scrape data files

---
+++ Scrape Data/TeamClass.js
@@ -0,0 +1,60 @@
+class Team {
+  constructor(name) {
+    this.name = name;
+    this.metrics = {}; // Object to store performance metrics
+  }
+
+  addMetric(metric, value) {
+    this.metrics[metric] = value;
+  }
+}
+class result {
+  constructor(homeTeamGoal, awayTeamGoal) {
+    this.homeTeamGoal = homeTeamGoal;
+    this.awayTeamGoal = awayTeamGoal;
+  }
+}
+class Match {
+  constructor(firstTeam, secondTeam, result) {
+    this.firstTeam = firstTeam;
+    this.secondTeam = secondTeam;
+    this.result = result; // Object containing match result (e.g., goals scored, goals conceded)
+  }
+}
+
+class TeamGraph {
+  constructor() {
+    this.teams = []; // Array to store teams
+    this.matches = []; // Array to store match objects
+  }
+
+  addTeam(teamName) {
+    if (!this.teams[teamName]) {
+      this.teams.push(new Team(String(teamName)));
+    }
+  }
+  addMatch(firstTeam, secondTeam, fisrtTeamGoal, secondTeamGoal) {
+    this.matches.push(
+      new Match(
+        firstTeam,
+        secondTeam,
+        new result(fisrtTeamGoal, secondTeamGoal)
+      )
+    );
+  }
+}
+// let nouveauGraph = new TeamGraph();
+// nouveauGraph.addTeam("Tottenham Hotspur");
+// nouveauGraph.addTeam("Manchester United");
+// nouveauGraph.addTeam("Chelsea");
+// nouveauGraph.addTeam("Manchester City");
+// nouveauGraph.addMatch("Chelsea", "Manchester United", 4, 3);
+// nouveauGraph.addMatch("Manchester United", "Manchester City", 1, 3);
+// nouveauGraph.addMatch("Manchester United", "Manchester City", 0, 4);
+// nouveauGraph.addMatch("Manchester City", "Manchester United", 2, 0);
+// console.log(nouveauGraph);
+// for (let i = 0; i < nouveauGraph.matches.length; i++) {
+//   console.log(nouveauGraph.matches[i].result);
+// }
+
+module.exports = TeamGraph;
