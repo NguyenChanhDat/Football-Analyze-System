0a58ea716e960b38e63780ee9d2b3e7d7a9850c3
nguyenchanhdat <chanhdat2504@gmail.com>
2024-04-24 18:36:32 +0700

    add scrapeWeb

---
+++ Scrape Data/scrapePlayerData.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+axios
+  .get("https://footystats.org/clubs/dpm-nam-dinh-fc-4019#")
+  .then(({ data }) => {
+    const $ = cheerio.load(data);
+
+    const playerNames = $("p.col-lg-6.ellipses .semi-bold")
+      .map((_, product) => {
+        const $product = $(product);
+        return $product.text();
+      })
+      .toArray();
+    console.log(playerNames);
+  });
